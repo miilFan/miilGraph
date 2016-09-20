c5cd1b41f617b8862b98f104bf88c22d54836f11
daiiz <da.iz.mg@gmail.com>
2016-09-20 19:58:54 +0900

    ウェブアプリを参照する

---
+++ ext/miilClient/launch.js
@@ -0,0 +1,14 @@
+document.getElementById('mc').addEventListener('click', function () {
+    var w = 488;
+    var h = 668;
+    var l = window.screenX + (document.body.clientWidth / 2) - (w / 2);
+    var t = window.screenY + (document.body.clientHeight / 2) - (h / 2);
+    chrome.windows.create({
+        url: 'http://daiiz-apps.appspot.com/miilclient',
+        type: 'popup',
+        width: w,
+        height: h,
+        top: Math.round(t),
+        left: Math.round(l)
+    });
+});
