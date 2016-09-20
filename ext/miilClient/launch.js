19fca43dfdf94bf4b67d2d907ffbb26ac6a8a1d7
daiiz <da.iz.mg@gmail.com>
2016-09-20 20:25:31 +0900

    update ext version

--- ext/miilClient/launch.js
+++ ext/miilClient/launch.js
@@ -1,6 +1,6 @@
 document.getElementById('mc').addEventListener('click', function () {
     var w = 488;
-    var h = 668;
+    var h = 668; //+ 22
     var l = window.screenX + (document.body.clientWidth / 2) - (w / 2);
     var t = window.screenY + (document.body.clientHeight / 2) - (h / 2);
     chrome.windows.create({
